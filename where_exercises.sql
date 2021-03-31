ee169bcaec2266a62693b64047172e2cabdbd859
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-03-31 12:37:18 -0500

    adding where exercise

---
+++ where_exercises.sql
@@ -0,0 +1,42 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+AND (
+    gender = 'M'
+    );
+
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('E%');
+
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('%q%');
+
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('E%')
+OR (
+    last_name LIKE ('%e')
+    );
+
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('E%')
+   AND (
+        last_name LIKE ('%e')
+    );
+
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('%q%')
+AND (
+        last_name ! ('%qu%')
+    );
