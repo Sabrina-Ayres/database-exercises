3abc733f28f0d1ff62316ef96825d4b51d52ce76
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-04-06 11:24:00 -0500

    updating database design lecture exercise

--- database_design_lecture.sql
+++ database_design_lecture.sql
@@ -52,7 +52,7 @@ VALUES ('J. K.', 'Rowling'),
 
 INSERT INTO quotes(author_id, content)
 VALUES ((select id from authors where first_name = 'J. K.' and last_name = 'Rowling'),
-    'It is our choices... that show what we truly are, far more than our abilities.');
+        'It is our choices... that show what we truly are, far more than our abilities.');
 INSERT INTO quotes(author_id, content)
 VALUES ((select id from authors where first_name = 'J. K.' and last_name = 'Rowling'),
         'I sometimes have a tendency to walk on the dark side.');
@@ -72,3 +72,38 @@ VALUES ((select id from authors where first_name = 'Delia' and last_name = 'Owen
         'There are some who can live without wild things, and some who cannot.');
 
 
+
+CREATE TABLE topics
+(
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO topics(name)
+VALUES ('Inspirational'),
+       ('Life'),
+       ('Motivational'),
+       ('Choices'),
+       ('Love'),
+       ('Relationships'),
+       ('Self-Awareness');
+
+
+SELECT *
+FROM topics;
+
+
+CREATE TABLE quote_topic
+(
+    quote_id INTEGER UNSIGNED NOT NULL,
+    topic_id INTEGER UNSIGNED NOT NULL,
+    FOREIGN KEY (quote_id) REFERENCES quotes (id),
+    FOREIGN KEY (topic_id) REFERENCES topics (id)
+);
+
+INSERT INTO quote_topic(quote_id, topic_id)
+VALUES (1, 7), (1, 3), (2, 2), (3, 2), (4, 5), (4, 6), (5, 2), (5, 7), (6, 2), (6, 7);
+
+SELECT *
+FROM quote_topic;
